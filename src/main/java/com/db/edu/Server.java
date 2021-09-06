f13555da4c4b2c4d37a876124444d720d87d06ea
Vsevolod Fedorov <fsdoc151@mail.ru>
2021-09-06 11:50:00 +0300

    Implemented User Map.

--- src/main/java/com/db/edu/Server.java
+++ src/main/java/com/db/edu/Server.java
@@ -3,17 +3,26 @@ package com.db.edu;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 public class Server {
     public static void main(String[] args) {
+
+        HashMap<SocketAddress, String> connections = new HashMap<>();
+
         try (final ServerSocket listener = new ServerSocket(10_000)) {
             try (
                     final Socket connection = listener.accept();
                     final DataInputStream input = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
                     final DataOutputStream output = new DataOutputStream(new BufferedOutputStream(connection.getOutputStream()))
             ) {
+                SocketAddress address = connection.getRemoteSocketAddress();
+                if (address != null && !connections.containsKey(address)) {
+                    connections.put(address, address.toString());
+                }
 
                 while (true) {
                     final String message = input.readUTF();
@@ -21,7 +30,7 @@ public class Server {
                     if ( message.startsWith("/snd") ) {
                         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
                         LocalDateTime now = LocalDateTime.now();
-                        String processedMessage = dtf.format(now) + " UserName " + message.replaceFirst("/snd ", "");
+                        String processedMessage = "[" + dtf.format(now) + "] " + connections.get(address) + " : " + message.replaceFirst("/snd ", "");
                         output.writeUTF(processedMessage);
                         output.flush();
                     }
