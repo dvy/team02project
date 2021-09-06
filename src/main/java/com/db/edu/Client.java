3994d75f25c0f744e748684e48ddb40f58c4d013
asvodovatov <a.s.vodovatov@gmail.com>
2021-09-06 11:16:11 +0300

    Client simple version

--- src/main/java/com/db/edu/Client.java
+++ src/main/java/com/db/edu/Client.java
@@ -1,4 +1,25 @@
 package com.db.edu;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) {
+
+        try (
+                final Socket connection = new Socket("localhost", 10_000);
+                final DataInputStream input = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
+                final DataOutputStream output = new DataOutputStream(new BufferedOutputStream(connection.getOutputStream()))
+        ) {
+            Scanner in = new Scanner(System.in);
+            output.writeUTF(in.nextLine()); // protocol
+            output.flush();
+
+            System.out.println(input.readUTF());
+
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+    }
 }
