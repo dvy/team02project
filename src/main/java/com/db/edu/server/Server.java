314de758a206a8a8d26ca12d295b0dca19dc6cc1
Vsevolod Fedorov <fsdoc151@mail.ru>
2021-09-07 15:22:52 +0300

    Changed command format for server exit.

--- src/main/java/com/db/edu/server/Server.java
+++ src/main/java/com/db/edu/server/Server.java
@@ -42,7 +42,7 @@ public class Server {
                 while (true){
                     if (scanner.hasNext()) {
                         String s = scanner.next();
-                        if (s.equals("/quit")) {
+                        if (s.equals("/exit")) {
                             controllerThread.interrupt();
                             try {
                                 listener.close();
